ca5f203d2a59be111413a8c8bf4e52ddb909ce6e
thinkport <thinkport@live.cn>
2020-04-17 17:31:10 +0800

    Add files via upload

--- src/emqx_plugin_device_app.erl
+++ src/emqx_plugin_device_app.erl
@@ -11,11 +11,12 @@
         ]).
 
 start(_StartType, _StartArgs) ->
-    {ok, Sup} = emqx_plugin_device_sup:start_link(),
-    emqx_plugin_device:load(application:get_all_env()),
-    {ok, Sup}.
+    {ok, Sup} = emqx_plugin_device_sup:start_link().
+    %%emqx_plugin_device:load(application:get_all_env()),
+    %%{ok, Sup}.
 
 stop(_State) ->
-	eredis_cluster:stop_pool(?APP),
-    emqx_plugin_device:unload().
+	ok.
+	%%eredis_cluster:stop_pool(?APP),
+    %%emqx_plugin_device:unload().
 
