9a745ac9c7223721885dd8bb9cb2cc5e0aa7fbaf
thinkport <thinkport@live.cn>
2020-04-17 19:51:16 +0800

    Add files via upload

--- src/emqx_plugin_device_sup.erl
+++ src/emqx_plugin_device_sup.erl
@@ -32,7 +32,7 @@ init([]) ->
     {ok, {{one_for_one, 10, 100}, pool_spec(Server)}}.
 	
 pool_spec(Server) ->
-	io:format("Server ~s~n", [Server]),
+	io:format("Server ~p~n", [Server]),
     case proplists:get_value(type, Server) of
         cluster ->
            eredis_cluster:start_pool(?APP, Server),
