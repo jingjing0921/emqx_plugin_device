605168e34da0a1863aaaff4570f5be845634c625
thinkport <thinkport@live.cn>
2020-04-18 13:33:35 +0800

    Update emqx_plugin_device_sup.erl

--- src/emqx_plugin_device_sup.erl
+++ src/emqx_plugin_device_sup.erl
@@ -1,24 +1,6 @@
-%%--------------------------------------------------------------------
-%% Copyright (c) 2020 EMQ Technologies Co., Ltd. All Rights Reserved.
-%%
-%% Licensed under the Apache License, Version 2.0 (the "License");
-%% you may not use this file except in compliance with the License.
-%% You may obtain a copy of the License at
-%%
-%%     http://www.apache.org/licenses/LICENSE-2.0
-%%
-%% Unless required by applicable law or agreed to in writing, software
-%% distributed under the License is distributed on an "AS IS" BASIS,
-%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-%% See the License for the specific language governing permissions and
-%% limitations under the License.
-%%--------------------------------------------------------------------
-
 -module(emqx_plugin_device_sup).
 
 -behaviour(supervisor).
-
--include("emqx_plugin_device.hrl").
 
 -export([start_link/0]).
 
@@ -28,16 +10,4 @@ start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-    {ok, Server} = application:get_env(?APP, server),
-    {ok, {{one_for_one, 10, 100}, pool_spec(Server)}}.
-	
-pool_spec(Server) ->
-	io:format("Server ~p~n", [Server]),
-    case proplists:get_value(type, Server) of
-        cluster ->
-           eredis_cluster:start_pool(?APP, Server),
-            [];
-        _ ->
-            [ecpool:pool_spec(?APP, ?APP, emqx_plugin_device_cli, Server)]
-    end.
-
+    {ok, { {one_for_all, 0, 1}, []} }.
