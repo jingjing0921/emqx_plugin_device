3ed017831eada572dd2a889cfce501abf5aeb3e2
thinkport <thinkport@live.cn>
2020-04-18 14:02:53 +0800

    Update emqx_plugin_device_app.erl

--- src/emqx_plugin_device_app.erl
+++ src/emqx_plugin_device_app.erl
@@ -12,10 +12,9 @@
 
 start(_StartType, _StartArgs) ->
     {ok, Sup} = emqx_plugin_device_sup:start_link(),
-    %%emqx_plugin_device:load(application:get_all_env()),
+    emqx_plugin_device:load(application:get_all_env()),
     {ok, Sup}.
 
 stop(_State) ->
-	%%eredis_cluster:stop_pool(?APP),
+    eredis_cluster:stop_pool(?APP),
     emqx_plugin_device:unload().
-
