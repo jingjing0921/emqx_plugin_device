90ca1af2da2000d069478919f1e3082d2a34a0c3
thinkport <thinkport@live.cn>
2020-04-18 13:56:53 +0800

    Update emqx_plugin_device_sup.erl

--- src/emqx_plugin_device_sup.erl
+++ src/emqx_plugin_device_sup.erl
@@ -13,16 +13,14 @@ start_link() ->
 
 init([]) ->
     {ok, Server} = application:get_env(?APP, server),
-	io:format("Server ~p~n", [Server]),
     {ok, {{one_for_one, 10, 100}, pool_spec(Server)}}.
 	
 pool_spec(Server) ->
-	io:format("Server2 ~p~n", [Server]),
     case proplists:get_value(type, Server) of
         cluster ->
            eredis_cluster:start_pool(?APP, Server),
             [];
         _ ->
-			io:format("Server3 ~p~n", [Server]),
-            [ecpool:pool_spec(?APP, ?APP, emqx_plugin_device_cli, Server)]
+            [ecpool:pool_spec(?APP, ?APP, emqx_plugin_device_redis_cli, Server)]
     end.
+
