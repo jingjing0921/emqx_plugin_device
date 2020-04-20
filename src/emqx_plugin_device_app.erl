a31d230b02a658f2648bffc85734cdb58f7640cf
thinkport <thinkport@live.cn>
2020-04-20 20:14:42 +0800

    Update emqx_plugin_device_app.erl

--- src/emqx_plugin_device_app.erl
+++ src/emqx_plugin_device_app.erl
@@ -13,11 +13,12 @@
 start(_StartType, _StartArgs) ->
     {ok, Sup} = emqx_plugin_device_sup:start_link(),
 	{ok, Timeout} = application:get_env(?APP, query_timeout),
-	io:format("~p~n", [application:get_all_env(emqx_plugin_device)]),
+	[_|{query_timeout, Timeout}|_]application:get_all_env(emqx_plugin_device),
+	io:format("~s~n", [Timeout]),
+	
     emqx_plugin_device:load(Timeout),
     {ok, Sup}.
 
 stop(_State) ->
 	eredis_cluster:stop_pool(?APP),
     emqx_plugin_device:unload().
-
