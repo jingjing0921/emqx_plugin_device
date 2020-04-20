ed38424d380c4b1522e64d75b6a755f5302bb76f
thinkport <thinkport@live.cn>
2020-04-20 20:07:09 +0800

    Update emqx_plugin_device_app.erl

--- src/emqx_plugin_device_app.erl
+++ src/emqx_plugin_device_app.erl
@@ -13,7 +13,7 @@
 start(_StartType, _StartArgs) ->
     {ok, Sup} = emqx_plugin_device_sup:start_link(),
 	{ok, Timeout} = application:get_env(?APP, query_timeout),
-	io:format("~p~n", [application:get_all_env()]);
+	io:format("~p~n", [application:get_all_env()]),
     emqx_plugin_device:load(Timeout),
     {ok, Sup}.
 
